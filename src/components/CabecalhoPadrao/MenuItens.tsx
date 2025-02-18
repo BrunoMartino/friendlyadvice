4b332e4f2e42ab828da8d65196991cd29e698264
Bruno Galvão Martins <bruno.martins@client-craft.com>
2025-02-18 10:46:38 -0300

    feat: adicionado navegação para geolocalização no menu Aplicativos

--- src/components/CabecalhoPadrao/MenuItens.tsx
+++ src/components/CabecalhoPadrao/MenuItens.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { GiExitDoor } from 'react-icons/gi';
-import { IoIosPaper } from 'react-icons/io';
+import { IoIosGlobe, IoIosPaper } from 'react-icons/io';
 import _remove from 'lodash/remove';
 
 export const menuItensAplicativos = () => {
@@ -8,11 +8,18 @@ export const menuItensAplicativos = () => {
 
   {
     process.env.REACT_APP_ENV === 'DEV' &&
-      result.push({
-        nome: 'Ordens de Serviço',
-        icone: <IoIosPaper />,
-        url: '/listagem/OrdensDeServico',
-      });
+      result.push(
+        {
+          nome: 'Ordens de Serviço',
+          icone: <IoIosPaper />,
+          url: '/listagem/OrdensDeServico',
+        },
+        {
+          nome: 'Geolocalização OS',
+          icone: <IoIosGlobe />,
+          url: '/geolocalizacao/OrdensDeServico',
+        },
+      );
   }
 
   return result;
